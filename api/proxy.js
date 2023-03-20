8103c7acde002472b698cafab5ad99e991927733
wuhannan <1242865419@qq.com>
2023-03-20 17:08:08 +0800

    proxy setting

--- api/proxy.js
+++ api/proxy.js
@@ -8,9 +8,9 @@ module.exports = (req, res) => {
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     // target 替换为你跨域请求的服务器 如： http://baidu.com
     if (req.url.startsWith('/api')) {
-        target = 'http://127.0.0.1:5000'
-        /* target = 'http://gmall-h5-api.atguigu.cn' */
-        req.url = req.headers.realPath;
+        /* target = 'http://127.0.0.1:5000' */
+        target = 'http://gmall-h5-api.atguigu.cn'
+        req.url = '/api' + req.headers.realPath;
     }
     /* /api/proxy?path=xx */
     /* path -> req -> /api/path */
